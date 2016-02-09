f2a45395a4567327c8c978c1a4dd7f9f203b4fde
nicolasrannou <npm.rannou@gmail.com>
2016-02-09 09:23:07 +0100

    fix baseURL

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,7 +20,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   if (window.location.port === '') {  // if production
     // Uncomment app.baseURL below and
     // set app.baseURL to '/your-pathname/' if running from folder in production
-    app.baseUrl = '/www.eunate.ch/';
+    //app.baseUrl = '/www.eunate.ch/';
   }
 
   app.displayInstalledToast = function() {
